36d4bec24b1d40a2beee7e54724d98f9c54f6173
minh-lyft <nguyenminh@lyft.com>
2026-04-04 20:05:16 -0400

    project set up

---
+++ src/data.ts
@@ -0,0 +1,16 @@
+export let datingTimeline = [
+    {
+        year: 2025,
+        events: [
+            {
+                date: '26-08-2025',
+                name: 'First text',
+            },
+            {
+                date: '01-09-2025',
+                name: 'First Met',
+                des: 'Chiang Mai Thai restaurant',
+            },
+        ],
+    },
+];
